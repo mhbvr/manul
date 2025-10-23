b940f482af9723224b3f24c44a4af33261ff9fd0
Mikhail Kulemin <mih@mrphs.cc>
2025-10-23 20:48:11 +0100

    Add more buckets to the requests histogram

--- client_loadtest/metrics.go
+++ client_loadtest/metrics.go
@@ -30,7 +30,7 @@ func NewMetrics() *Metrics {
 				Name: "loadtester_request_duration_seconds",
 				Help: "Request latency in seconds",
 				Buckets: []float64{
-					0.001, 0.002, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
+					0.001, 0.002, 0.004, 0.006, 0.008, 0.01, 0.02, 0.04, 0.06, 0.08, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 2.5, 5.0, 10.0,
 				},
 			},
 			[]string{"status", "runner_id"}, // "success" or "error", runner identifier
